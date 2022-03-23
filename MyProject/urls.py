31baf7dfa1755816e56c606f3dbe11bf10f73d4b
meghana-16 <86870665+meghana-16@users.noreply.github.com>
2022-03-23 19:43:56 +0530

    Update urls.py

--- MyProject/urls.py
+++ MyProject/urls.py
@@ -17,10 +17,19 @@ from pathlib import Path
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+from MyProject.settings import MEDIA_ROOT
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api_basic.urls')),
     path('accounts/', include('allauth.urls')),
     path('', TemplateView.as_view(template_name='todo/index.html'))
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}),
+
 ]
+urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
