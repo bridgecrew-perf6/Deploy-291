384d351a26c6b7ab04f422b5db326e3458c77b11
meghana-16 <86870665+meghana-16@users.noreply.github.com>
2022-03-23 20:09:27 +0530

    Update settings.py

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-rgu7nwsqvqgt5knwh0+w!s@w+i2xjznvzb5l^k23)e!shja&cg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','meghanapp.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','meghapp.herokuapp.com']
 
 
 # Application definition
